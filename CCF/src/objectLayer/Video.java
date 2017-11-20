1438dc444f0f1b8463b416db316e24d32bd06f20
jbstock <jbstock@uga.edu>
2017-11-20 10:53:37 -0500

    Video

--- CCF/src/objectLayer/Video.java
+++ CCF/src/objectLayer/Video.java
@@ -2,5 +2,26 @@ package objectLayer;
 
 public class Video {
 	private int id = 0;
+	private String embedURL = null;
 	
+	public Video(int id, String embedURL) {
+		this.id = id;
+		this.embedURL = embedURL;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmbedURL() {
+		return embedURL;
+	}
+
+	public void setEmbedURL(String embedURL) {
+		this.embedURL = embedURL;
+	}
 }
