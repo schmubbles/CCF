b340988efd6076752fe6ce5b56d4b8c47f402907
jbstock <jbstock@uga.edu>
2017-11-29 17:30:26 -0500

    db updates

--- CCF/src/persistLayer/DatabaseAccessConfiguration.java
+++ CCF/src/persistLayer/DatabaseAccessConfiguration.java
@@ -1,11 +1,11 @@
 package persistLayer;
 
 public class DatabaseAccessConfiguration {
-	static final String DB_DRIVE_NAME = "";
+	static final String DB_DRIVE_NAME = "org.mariadb.jdbc.Driver";
 	
-	static final String DB_CONNECTION_URL = "";
+	static final String DB_CONNECTION_URL = "jdbc:mariadb://student@vm408.nike.cs.uga.edu/CCF";
 	
-	static final String DB_CONNECTION_USERNAME = "";
+	static final String DB_CONNECTION_USERNAME = "root";
 	
 	static final String DB_CONNECTION_PASSWORD = "";
 }
