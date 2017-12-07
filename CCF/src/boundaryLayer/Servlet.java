8450cc305472ef735bd482b8a2171eb45080734c
jbstock <jbstock@uga.edu>
2017-12-07 16:57:49 -0500

    freemarker in Servlet

--- CCF/src/boundaryLayer/Servlet.java
+++ CCF/src/boundaryLayer/Servlet.java
@@ -1,10 +1,14 @@
 package boundaryLayer;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.text.Collator;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +17,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import logicLayer.logicLayer;
 import objectLayer.Player;
 
@@ -22,12 +29,27 @@ import objectLayer.Player;
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	static  String  templateDir = "";
+	static  String  resultTemplateName = "sign-up.ftl"; 
+	
+	private Configuration  cfg;
+	
+	public void init()
+	{
+	    // Prepare the FreeMarker configuration;
+	    // - Load templates from the WEB-INF/templates directory of the Web app.
+	    //
+		cfg = new Configuration();
+	    cfg.setServletContextForTemplateLoading(getServletContext(), "");
+	}
 
     /**
      * Default constructor. 
      */
     public Servlet() {
         // TODO Auto-generated constructor stub
+    	super();
     }
 
 	/**
@@ -45,7 +67,16 @@ public class Servlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 		logicLayer logicLayer = new logicLayer();
+
+		Template resultTemplate = null; 
+		BufferedWriter toClient = null;
 		
+		resultTemplate = cfg.getTemplate( resultTemplateName );
+		
+	    toClient = new BufferedWriter(new OutputStreamWriter(response.getOutputStream(), resultTemplate.getEncoding())); 
+		
+	    response.setContentType("text/html; charset=" + resultTemplate.getEncoding());
+
 		HttpSession httpSession = request.getSession();
 		System.out.println(httpSession.getAttribute("ssid"));
 		
@@ -60,6 +91,21 @@ public class Servlet extends HttpServlet {
 		});
 		
 		System.out.println(players.toString());
+		
+		Map<String,Object> root = new HashMap<String,Object>();
+		 
+		 root.put("Players", players); 
+		 
+		 try 
+		 {
+			resultTemplate.process(root, toClient);
+		 } 
+		 catch (TemplateException e) 
+		 {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
 	}
 	
 	
