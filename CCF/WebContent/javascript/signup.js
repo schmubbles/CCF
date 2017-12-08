ce3d6996b0c05940d6375ee7d4874576abbe3b4b
Eric Holbrook <ericalanholbrook@gmail.com>
2017-12-08 13:12:04 -0500

    Added jquery functionality for most pages. Mostly to populate player and tournament html

--- CCF/WebContent/javascript/signup.js
+++ CCF/WebContent/javascript/signup.js
@@ -1,26 +1,35 @@
-/**
- * 
- */
-
 
 $(document).ready(function(){
-	$('#signUp').submit(function(){
+
+	loadRegisteredPlayers();
+
+	$('#signupForm').submit(function(event){
+		event.preventDefault();
+
 		$.ajax({
 			url:"signup",
 			type: "POST",
 			dataType: "json",
-			data: $("#signUp").serialize(),
+			data: $("#signupForm").serialize(),
 			success: function(data){
-				
+
 			}
 			
 			
 		});
 		return false;
-		
-		
-		
 	});
 	
+	//loads players who have signed up for the tournament
+	function loadRegisteredPlayers(){
+		var requestURL = "Servlet?loadRegisteredPlayers";
+		$.get(requestURL, function(result, response) {
+			$("#playerOrderedList").html("");
+			$.each(result, function(index, Player){
+				var html = '<li>' + Player.playerTag + '</li>';
+				$("#playerOrderedList").append(html);
+			});
+		});
+	}
 	
 });
