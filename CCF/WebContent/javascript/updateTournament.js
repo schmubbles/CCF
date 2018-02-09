8d8e8cdd38d03c3467b0582c3fdbdf1337c80b0c
schmubbles <schmubbles@users.noreply.github.com>
2018-02-08 22:22:05 -0500

    Add files via upload
    added html code for sumbitting a tournament either via file upload or manual upload

--- CCF/WebContent/javascript/updateTournament.js
+++ CCF/WebContent/javascript/updateTournament.js
@@ -1,5 +1,28 @@
+
 $(document).ready(function(){
-    
+   
+    $('.group').hide(); 
+
+    $("#selector").change(function(){
+        $('.group').hide();        
+        $('#'+$(this).val()).show();
+    });
+
+    $('#file-button').click(function(){
+        $('#my-file').click();
+    });
+
+    $("#fileEnterForm").submit(function(event) {
+        alert("sumbit");
+    });
+
+    $("#addPlayer").submit(function(){
+        $("#playerTable > tbody:last-child").append("<tr><td>" + $("#playerOne").val() + "</td><td>" + $("#playerTwo").val() + "</td></tr>");
+        $("#playerOne").val("");
+        $("#playerTwo").val("");
+        return false;
+    });
+
     $("#addTournament").submit(function(event){
         event.preventDefault();
 
